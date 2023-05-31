257c967494ec92ab4fbad3d9b02f088e5daad3e9
Xaviontt <xavion@list.ru>
2023-05-31 13:53:49 +0300

    Scheduler fix

--- src/main/java/blps/lab2/dao/UserRepository.java
+++ src/main/java/blps/lab2/dao/UserRepository.java
@@ -3,10 +3,17 @@ package blps.lab2.dao;
 import blps.lab2.model.domain.topic.Topic;
 import blps.lab2.model.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long>  {
+    @Transactional
+    @Modifying
+    @Query("update User u set u.remainingGrades = 100")
+    int updateRemainingGradesBy100();
     Optional<User> findByEmail(String email);
     Optional<User> findById(long id);
 }
--- src/main/java/blps/lab2/service/user/UserScheduler.java
+++ src/main/java/blps/lab2/service/user/UserScheduler.java
@@ -1,13 +1,27 @@
 package blps.lab2.service.user;
 
+import blps.lab2.dao.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+@Slf4j
+@Configuration
 @EnableScheduling
 public class UserScheduler {
 
-    @Scheduled(cron = "0 * * ? * *")
-    @Query("UPDATE User u SET u.remainingGrades = 10")
-    public void updateLikes(){}
+    private final UserRepository userRepository;
+
+    public UserScheduler(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Scheduled(fixedRate = 10000)
+    public void updateLikes(){
+        log.info("Cron Cron Cron");
+        userRepository.updateRemainingGradesBy100();
+        userRepository.saveAll(userRepository.findAll());
+    }
 }
